380142d493465566fbb1c374e5525a70a441243e
hennasingh <hennasingh04@gmail.com>
2021-07-10 22:19:47 +0100

    edited location activity and hard-coded 2 locations for starting app

--- app/src/main/java/com/geek/restaurants/LocationActivity.kt
+++ app/src/main/java/com/geek/restaurants/LocationActivity.kt
@@ -2,7 +2,6 @@ package com.geek.restaurants
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.mongodb.User
 import kotlinx.android.synthetic.main.activity_location.*
@@ -14,8 +13,18 @@ class LocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
+        
+        btn_manhattan.setOnClickListener{
+            openPartition("Manhattan")
+        }
+        
+        btn_brooklyn.setOnClickListener{
+            openPartition("Brooklyn")
+        }
+    }
 
-        initializeSpinnerData()
+    private fun openPartition(partition: String) {
+
     }
 
     override fun onStart() {
@@ -25,20 +34,6 @@ class LocationActivity : AppCompatActivity() {
         if(user == null){
             // if no user is currently logged in, start the login activity so the user can authenticate
             startActivity(Intent(this, LoginActivity::class.java))
-        }
-    }
-
-    private fun initializeSpinnerData() {
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.location_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            locSpinner.adapter = adapter
         }
     }
 
